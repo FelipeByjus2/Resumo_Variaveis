d3faa24a9eb97d5b682e1f2a5b037abebd3e33da
Felipe Lages <134104317+FelipeByjus2@users.noreply.github.com>
2024-12-01 10:56:51 -0300

    Add files via upload

---
+++ Variáveis.py
@@ -0,0 +1,15 @@
+# AS variáveis servem para armazenar uma informação
+variavel = 'informação'
+
+#Existem tipos de variáveis:
+#string(str) - Armazena um texto
+texto = 'Olá Mundo!'
+
+#inteiros(int) - Armazena numeros inteiros
+idade = 14
+
+#flutoantes(float) - Armazena numeros com parte decimal
+altura = 1.82
+
+#booleanos(bool) - Armazena valores lógicos, sendo eles 'True' e 'False'
+maior_de_idade = True
